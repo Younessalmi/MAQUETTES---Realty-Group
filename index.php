51c806f052fbbc0aa0eaf6f5ddeca438b9d39667
Younessalmi <younessalmi193@gmail.com>
2021-08-07 19:34:23 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Realty Group</title>
+    <link rel="stylesheet" href="./style.css"/>
+    <link rel="stylesheet" href="./w3.css"/>
+</head>
+<body>
+
+<header>
+    <nav>                        
+        <img src="./img/logo.png" style="width:80px;">
+
+                            <ul class="dropdown">
+                                <li class="dropbtn"> 
+                                    <a style="color:#33b0ec;font-size:20px;">Products</a>
+                                    <div class="dropdown-content">
+                                            <div style="float:left;margin-left: 90px;">
+                                                <h5 style="color:white;font-family:Arial;font-size:18px;color:brown;">Learning & Games</h5>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">snoopydoo</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">falling Angel</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">sull maker</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Wave Master</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Golf Pro</a>
+                                            </div>
+                                            <div style="float:right;margin-right:90px;text-lign:center;">
+                                                <h5 style="color:white;font-family:Arial;font-size:18px;color:brown;">Utilities</h5>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Godget Finder</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Green Tree Express</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Green The proo</a> 
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Wobbler 3.0</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Coolkid</a>
+                                            </div>
+                                    </div>
+                               </li>
+                                <li> <a style="color:#33b0ec;font-size:20px;">Products</a>
+                                    <div class="dropdown-content">
+                                            <div style="float:left;margin-left: 90px;">
+                                                <h5 style="color:white;font-family:Arial;font-size:18px;color:brown;">Learning & Games</h5>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">snoopydoo</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">falling Angel</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">sull maker</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Wave Master</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Golf Pro</a>
+                                            </div>
+                                            <div style="float:right;margin-right:90px;text-lign:center;">
+                                                <h5 style="color:white;font-family:Arial;font-size:18px;color:brown;">Utilities</h5>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Godget Finder</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Green Tree Express</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Green The proo</a> 
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Wobbler 3.0</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Coolkid</a>
+                                            </div>
+                                    </div> </li>
+                                <li> <a style="color:#33b0ec;font-size:20px;">Application</a>
+                                    <div class="dropdown-content">
+                                            <div style="float:left;margin-left: 90px;">
+                                                <h5 style="color:white;font-family:Arial;font-size:18px;color:brown;">Learning & Games</h5>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">snoopydoo</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">falling Angel</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">sull maker</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Wave Master</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Golf Pro</a>
+                                            </div>
+                                            <div style="float:right;margin-right:90px;text-lign:center;">
+                                                <h5 style="color:white;font-family:Arial;font-size:18px;color:brown;">Utilities</h5>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Godget Finder</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Green Tree Express</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Green The proo</a> 
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Wobbler 3.0</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Coolkid</a>
+                                            </div>
+                                    </div> </li>
+                                <li> <a style="color:#33b0ec;font-size:20px;">Projets</a>
+                                    <div class="dropdown-content">
+                                            <div style="float:left;margin-left: 90px;">
+                                                <h5 style="color:white;font-family:Arial;font-size:18px;color:brown;">Learning & Games</h5>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">snoopydoo</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">falling Angel</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">sull maker</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Wave Master</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Golf Pro</a>
+                                            </div>
+                                            <div style="float:right;margin-right:90px;text-lign:center;">
+                                                <h5 style="color:white;font-family:Arial;font-size:18px;color:brown;">Utilities</h5>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Godget Finder</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Green Tree Express</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Green The proo</a> 
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Wobbler 3.0</a>
+                                                <a href="#" style="color:white;font-family:Arial;font-size:10px;">Coolkid</a>
+                                            </div>
+                                    </div> </li>
+
+                            </ul>
+    </nav>
+
+</header>
+
+<div style="padding: 0px 90px;"> <hr style="background-color:#33b0ec;"> 
+</div>
+
+    <div style="margin:0 300px;">
+
+         <h3><b>Lorem Ipsum</b></h3>
+
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ante irure dolor in reprehenderit in voluptate velit esse cillum
+            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ante irure dolor in reprehenderit in voluptate velit esse cillum
+           dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+           <h3><b>Lorem Ipsum</b></h3>
+
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ante irure dolor in reprehenderit in voluptate velit esse cillum
+           dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+           <h3><b>Lorem Ipsum</b></h3>
+
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ante irure dolor in reprehenderit in voluptate velit esse cillum
+           dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    </div>
+
+        <footer>
+            <div style="text-align: center;background-color:#33b0ec;margin: 0 100px;">
+                <img style="width:70px;margin: 12px;" src="./img/footer.png">
+                <div>
+                    <p style="text-align:center;font-size:12px;color:white;">
+                    Cet exercice de <a href="#">3W Academy</a> est mis à disposition <a href="#">pour l'usage personnel des étudiants, Pas de Rediffusion - Attribution - 
+                    Pas d'Utilisation Commerciale - Pas de Modification - International.</a><br>Les autorisations au-delà du champ de cette licence peuvent être obtenues auprès de 
+                    <a href="mailto:contact@3wa.fr"> contact@3wa.fr</a>. Les maquettes ont été réalisées par Justine Muller.</p>
+                </div>
+            </div>
+        </footer>
+</body>
+</html>
